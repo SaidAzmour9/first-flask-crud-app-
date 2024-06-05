4599b86e9acc390b7a69ca20c5797380a4eb39cc
Said Azmour <azmour2016maroc@gmail.com>
2024-06-05 14:17:52 +0100

    add db

--- website/views.py
+++ website/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint,Flask,render_template,request,redirect,url_for,flash,session
+import sqlite3
 
 
 views = Blueprint('views',__name__)
 @views.route('/')
 def home():
-    return render_template('home.html')
+    con=sqlite3.connect("database.db")
+    con.row_factory=sqlite3.Row
+    cur=con.cursor()
+    cur.execute("select * from Livres")
+    data = cur.fetchall()
+    print(data)
+    return render_template('home.html',datas=data)
+
+@views.route('/add')
+def add():
+    return render_template('add.html')
+
+@views.route('/edit')
+def edit():
+    return render_template('edit.html')
